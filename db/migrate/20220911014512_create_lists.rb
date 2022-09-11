febeeeee6157d23cbd742bbf3263441a944d4a76
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2022-09-11 03:18:48 +0000

    [Start] 新規作成

---
+++ db/migrate/20220911014512_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lists do |t|
+
+      t.string :title
+      t.string :boby
+
+      t.timestamps
+    end
+  end
+end
